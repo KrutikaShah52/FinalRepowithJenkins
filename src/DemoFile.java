af3ff13a5789b1d0ccb66bf80b046c9408c3f9fb
KrutikaShah52 <krutiikashah5218@gmail.com>
2018-03-15 14:34:35 +0530

    third commit changes

--- src/DemoFile.java
+++ src/DemoFile.java
@@ -1,7 +1,6 @@
 
 
 public class DemoFile {
-	System.out.println("Starting ..................");
 	public int sum(int var1, int var2) {
 		        System.out.println("Adding values: " + var1 + " + " + var2);
 		        return var1 + var2;
