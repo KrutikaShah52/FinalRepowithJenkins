a4a5a96fabadb81af8d6abb75cc953d6a3cd7962
KrutikaShah52 <krutiikashah5218@gmail.com>
2018-03-15 11:45:03 +0530

    second commit

--- src/DemoFile.java
+++ src/DemoFile.java
@@ -1,17 +1,7 @@
 
 
 public class DemoFile {
-//	    public static void main(String args[])
-//	    {
-//	        Scanner scan = new Scanner(System.in);
-//	        int a, b, total;
-//	        System.out.println("Enter first number");
-//	        a = scan.nextInt();
-//	        System.out.println("Enter second number");
-//	        b = scan.nextInt();
-//	        scan.close();
-//	    }
-
+	System.out.println("Starting ..................");
 	public int sum(int var1, int var2) {
 		        System.out.println("Adding values: " + var1 + " + " + var2);
 		        return var1 + var2;
